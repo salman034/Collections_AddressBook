e5d5e81aa1af8d6d6ed2e8441dadc81eef23e996
unknown <you@example.com>
2022-04-24 07:04:19 +0530

    Added: Problems Based On Collections AddessBook

---
+++ src/com/bl/collections/Main.java
@@ -0,0 +1,7 @@
+package com.bl.collections;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Collections Address Book Problems!");
+    }
+}
