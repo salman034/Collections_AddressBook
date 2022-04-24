2f3d65bf6d52942c21ed7b8448ef48c1b5b7429f
unknown <you@example.com>
2022-04-24 07:11:55 +0530

    Added: Ability to create contacts in AddressBook

--- src/com/bl/collections/Main.java
+++ src/com/bl/collections/Main.java
@@ -2,6 +2,16 @@ package com.bl.collections;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome To The Collections Address Book Problems!");
+
+        PersonDetails person = new PersonDetails();
+        person.firstName("Salman");
+        person.lastName("Pasha");
+        person.Address("BTM 1st Stage");
+        person.City("Bangalore");
+        person.State("Karnataka");
+        person.ZipCode(560029);
+        person.PhoneNumber("9845715264");
+        person.Email("mks@gmail.com");
+        System.out.println(person.toString());
     }
 }
